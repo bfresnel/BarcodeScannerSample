5b6f72f09a2fae854541f9802f6269f61c95af89
bfresnel <benoit.fresnel.1@gmail.com>
2019-11-28 00:59:51 +0100

    Preparing call for the camera
    Camera is a deprecated API for Android v29. We have now to use Camera2. So I need to step up on this new API...

--- app/src/main/java/com/bfr/barcodescannersample/MainActivity.java
+++ app/src/main/java/com/bfr/barcodescannersample/MainActivity.java
@@ -1,13 +1,17 @@
 package com.bfr.barcodescannersample;
 
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +20,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        Button cameraButton = findViewById(R.id.cameraButton);
         setSupportActionBar(toolbar);
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CameraManager cam = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+                try {
+                    if (cam != null) {
+                        String[] idList = cam.getCameraIdList();
+                        for(int i=0; i < idList.length;i++){
+                            cam.getCameraCharacteristics(idList[i]);
+                        }
+                    }
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
