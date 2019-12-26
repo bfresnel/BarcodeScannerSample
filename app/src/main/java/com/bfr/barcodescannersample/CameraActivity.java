f425ad4b5f2ef8dba108ececd6a0d85918b6d1b9
bfresnel <benoit.fresnel.1@gmail.com>
2019-12-26 20:51:28 +0100

    Cleaning for better understanding

--- app/src/main/java/com/bfr/barcodescannersample/CameraActivity.java
+++ app/src/main/java/com/bfr/barcodescannersample/CameraActivity.java
@@ -2,40 +2,17 @@ package com.bfr.barcodescannersample;
 
 import android.Manifest;
 import android.content.Context;
-import android.content.pm.PackageManager;
-import android.graphics.Camera;
-import android.graphics.SurfaceTexture;
-import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
-import android.hardware.camera2.params.StreamConfigurationMap;
-import android.os.Handler;
-import android.os.HandlerThread;
-import android.util.Size;
-import android.view.Surface;
-import android.view.TextureView;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-
-import java.io.BufferedInputStream;
-import java.io.IOException;
 
 public class CameraActivity extends AppCompatActivity {
 
     public CameraManager cameraManager;
     public int cameraFacing;
     private static final int CAMERA_REQUEST_CODE = 2000;
-    public String cameraId;
-    private CameraDevice cameraDevice;
-
-    public TextureView textureView;
-    private TextureView.SurfaceTextureListener surfaceTextureListener;
-    private BufferedInputStream cameraCaptureSession;
-    private HandlerThread backgroundThread;
-    private Handler backgroundHandler;
-    private CameraDevice.StateCallback stateCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,127 +24,7 @@ public class CameraActivity extends AppCompatActivity {
         cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
         cameraFacing = CameraCharacteristics.LENS_FACING_BACK;
 
-        CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
-            @Override
-            public void onOpened(CameraDevice cameraDevice) {
-                CameraActivity.this.cameraDevice = cameraDevice;
-                // createPreviewSession();
-            }
 
-            @Override
-            public void onDisconnected(CameraDevice cameraDevice) {
-                cameraDevice.close();
-                CameraActivity.this.cameraDevice = null;
-            }
 
-            @Override
-            public void onError(CameraDevice cameraDevice, int error) {
-                cameraDevice.close();
-                CameraActivity.this.cameraDevice = null;
-            }
-        };
-
-        surfaceTextureListener = new TextureView.SurfaceTextureListener(){
-
-            @Override
-            public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int i, int i1) {
-                setUpCamera();
-                openCamera();
-            }
-
-            @Override
-            public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int i, int i1) {
-
-            }
-
-            @Override
-            public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
-                return false;
-            }
-
-            @Override
-            public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
-
-            }
-        };
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        textureView = findViewById(R.id.textureView);
-        openBackgroundThread();
-        if (textureView.isAvailable()) {
-            setUpCamera();
-            openCamera();
-        } else {
-            textureView.setSurfaceTextureListener(surfaceTextureListener);
-        }
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        try {
-            closeCamera();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        closeBackgroundThread();
-    }
-
-    private void setUpCamera(){
-        try{
-            for (String cameraId : cameraManager.getCameraIdList()) {
-                CameraCharacteristics cameraCharacteristics =
-                        cameraManager.getCameraCharacteristics(cameraId);
-
-                if (cameraCharacteristics.get(CameraCharacteristics.LENS_FACING) == cameraFacing) {
-                    StreamConfigurationMap streamConfigurationMap =
-                            cameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
-                    Size[] previewSize = streamConfigurationMap.getOutputSizes(SurfaceTexture.class);
-                    this.cameraId = cameraId;
-                }
-            }
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void openCamera(){
-        try {
-            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
-                    == PackageManager.PERMISSION_GRANTED) {
-                cameraManager.openCamera(cameraId, stateCallback, backgroundHandler);
-            }
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void openBackgroundThread() {
-        backgroundThread = new HandlerThread("camera_background_thread");
-        backgroundThread.start();
-        backgroundHandler = new Handler(backgroundThread.getLooper());
-    }
-
-    private void closeCamera() throws IOException {
-        if (cameraCaptureSession != null) {
-            cameraCaptureSession.close();
-            cameraCaptureSession = null;
-        }
-
-        if (cameraDevice != null) {
-            cameraDevice.close();
-            cameraDevice = null;
-        }
-    }
-
-    private void closeBackgroundThread() {
-        if (backgroundHandler != null) {
-            backgroundThread.quitSafely();
-            backgroundThread = null;
-            backgroundHandler = null;
-        }
     }
 }
